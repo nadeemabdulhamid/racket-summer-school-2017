d8980e98f0a19c7bc1876e2ec63d4bd006c5530e
Nadeem Abdul Hamid <nadeem@acm.org>
2017-07-03 22:00:44 -0400

    jsonic drracket integration

---
+++ beautiful-racket/jsonic/colorer.rkt
@@ -0,0 +1,56 @@
+#lang racket
+
+(require brag/support syntax-color/racket-lexer
+         racket/contract
+         (only-in br/list values->list))
+
+(define jsonic-lexer
+  (lexer
+   [(eof) (values lexeme 'eof #f #f #f)]
+   [(:or "[" "]" "{" "}")
+    (values lexeme 'parenthesis
+            (if (or (equal? lexeme "{")
+                    (equal? lexeme "[")) '|(| '|)|)
+            (pos lexeme-start) (pos lexeme-end))]
+   [(:or "@$" "$@")
+    (values lexeme 'parenthesis
+            (if (equal? lexeme "@$") '|(| '|)|)
+            (pos lexeme-start) (pos lexeme-end))]
+   [(from/stop-before "//" "\n")
+    (values lexeme 'comment #f
+            (pos lexeme-start) (pos lexeme-end))]
+   [any-char
+    (values lexeme 'string #f
+            (pos lexeme-start) (pos lexeme-end))]))
+
+(define (color-jsonic port offset racket-coloring-mode?)
+  (cond
+    [(or (not racket-coloring-mode?)
+         (equal? (peek-string 2 0 port) "$@"))
+     (define-values (str cat paren start end)
+       (jsonic-lexer port))
+     (define switch-to-racket-mode (equal? str "@$"))
+     (values str cat paren start end 0 switch-to-racket-mode)]
+    [else
+     (define-values (str cat paren start end)
+       (racket-lexer port))
+     (values str cat paren start end 0 #t)]))
+
+(provide (contract-out
+          [color-jsonic
+           (input-port? exact-nonnegative-integer? boolean?
+                        . -> . (values
+                                (or/c string? eof-object?)
+                                symbol?
+                                (or/c symbol? #f)
+                                (or/c exact-positive-integer? #f)
+                                (or/c exact-positive-integer? #f)
+                                exact-nonnegative-integer?
+                                boolean?))]))
+
+(module+ test
+  (require rackunit)
+  (check-equal? (values->list
+                 (color-jsonic (open-input-string "x") 0 #f))
+                (list "x" 'string #f 1 2 0 #f)))
+
--- beautiful-racket/jsonic/my-test.rkt
+++ beautiful-racket/jsonic/my-test.rkt
@@ -1,11 +1,15 @@
 #lang jsonic
 
-{ "name" : "Nadeem",
+{ "name" : "Nadeem",   // full name
   "yob" : @$ (+ 1900 77) $@,
-  "data" : [ null, @$ (string-append "41"   ; ignore this racket comment
-                                     (substring
-                                      (number->string (exact->inexact (/ 4 10)))
-                                      1)) $@, true ]
-  }
-// good
-
+  "data" :
+  [
+    null, @$ (string-append "41"   ; ignore this racket comment
+    (substring
+    (number->string (exact->inexact (/ 4 10)))
+    1)) $@, true,
+    @$ "test" $@
+  ]
+}
+  // good
+  
